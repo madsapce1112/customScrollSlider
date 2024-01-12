becbabeb0ff2954a920b1f22948053279bf195ef
markovicstef <markovicstef998@gmail.com>
2024-01-11 18:56:00 -0800

    feat: add scroll line and animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,129 @@
 "use client";
 import gsap from "gsap";
-import { Draggable, ScrollTrigger } from "gsap/all";
-import { useEffect, useLayoutEffect, useRef } from "react";
+import { ScrollToPlugin, ScrollTrigger } from "gsap/all";
+import { useEffect, useLayoutEffect, useState } from "react";
 
-gsap.registerPlugin(Draggable, ScrollTrigger);
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
 export default function Home() {
+  const [scrollProgress, setScrollProgress] = useState<string>("");
+  const [maxScroll, setMaxScroll] = useState<number>(0);
   const useIsomorphicLayoutEffect =
     typeof window !== "undefined" ? useLayoutEffect : useEffect;
 
   useIsomorphicLayoutEffect(() => {
-    let scrollTween = gsap.to("#customScrollbar", {
-      y: 300,
-      ease: "none",
+    gsap.to("#customScrollbar", {
       scrollTrigger: {
         start: 0,
         end: "max",
-        scrub: 1,
-      },
-    });
-
-    Draggable.create("#customScrollbar", {
-      type: "y",
-      bounds: "#customScroll",
-      throwProps: true,
-      onPress() {
-        scrollTween.scrollTrigger.disable(false);
-      },
-      onDrag() {
-        let progress = gsap.utils.normalize(this.minY, this.maxY, this.y);
-        let to = 15000 * progress;
-        scrollTo(0, to);
-      },
-      onRelease() {
-        let progress = gsap.utils.normalize(this.minY, this.maxY, this.y);
-        scrollTween.scrollTrigger.enable();
-        scrollTween.progress(progress);
+        onUpdate: (self) => {
+          setMaxScroll(self.end);
+          setScrollProgress(self.progress.toFixed(2));
+        },
       },
     });
   }, []);
 
+  const handleStepClick = (progress: number) => {
+    gsap.to(window, { duration: 1, scrollTo: { y: progress * maxScroll } });
+  };
+
   return (
-    <main className="min-h-screen relative">
-      <div style={{ height: "15000px" }}></div>
+    <main className="min-h-screen relative bg-white">
+      <div style={{ height: "15000px" }} id="allTags"></div>
 
       <div
         id="customScroll"
-        className="fixed right-10 top-0 z-[99999999px] h-80 w-12 bg-orange-400"
+        className="fixed left-10 top-10 h-96 flex flex-col justify-between after:absolute after:top-0 after:bottom-0  after:start-3.5 after:w-px after:-translate-x-[0.5px] after:bg-gray-300"
       >
-        <div id="customScrollbar" className="h-12 w-12 bg-red-600"></div>
+        <hr
+          className="bg-blue-700 w-0.5 start-[13px] absolute transition-all duration-500"
+          style={{ height: `${Number(scrollProgress) * 100}%` }}
+        />
+        <div className="flex gap-x-3" onClick={() => handleStepClick(0)}>
+          <div className="relative z-10 w-7 h-7 flex justify-center items-center">
+            <div
+              className={`w-2 h-2 rounded-full bg-white border-2  group-hover:border-gray-600 ${
+                Number(scrollProgress) >= 0
+                  ? "border-blue-700"
+                  : "border-gray-300"
+              }`}
+            ></div>
+          </div>
+
+          <div className="grow pt-0.5">
+            <h3 className="flex gap-x-1.5 font-semibold text-gray-800">
+              Created "Preline in React" task
+            </h3>
+          </div>
+        </div>
+        <div className="flex gap-x-3" onClick={() => handleStepClick(0.25)}>
+          <div className="relative z-10 w-7 h-7 flex justify-center items-center">
+            <div
+              className={`w-2 h-2 rounded-full bg-white border-2  group-hover:border-gray-600 ${
+                Number(scrollProgress) >= 0.25
+                  ? "border-blue-700"
+                  : "border-gray-300"
+              }`}
+            ></div>
+          </div>
+
+          <div className="grow pt-0.5">
+            <h3 className="flex gap-x-1.5 font-semibold text-gray-800">
+              Release v5.2.0 quick bug fix
+            </h3>
+          </div>
+        </div>
+        <div className="flex gap-x-3" onClick={() => handleStepClick(0.5)}>
+          <div className="relative z-10 w-7 h-7 flex justify-center items-center">
+            <div
+              className={`w-2 h-2 rounded-full bg-white border-2  group-hover:border-gray-600 ${
+                Number(scrollProgress) >= 0.5
+                  ? "border-blue-700"
+                  : "border-gray-300"
+              }`}
+            ></div>
+          </div>
+
+          <div className="grow pt-0.5">
+            <h3 className="flex gap-x-1.5 font-semibold text-gray-800">
+              Release v5.2.0 quick bug fix
+            </h3>
+          </div>
+        </div>
+        <div className="flex gap-x-3" onClick={() => handleStepClick(0.75)}>
+          <div className="relative z-10 w-7 h-7 flex justify-center items-center">
+            <div
+              className={`w-2 h-2 rounded-full bg-white border-2  group-hover:border-gray-600 ${
+                Number(scrollProgress) >= 0.75
+                  ? "border-blue-700"
+                  : "border-gray-300"
+              }`}
+            ></div>
+          </div>
+
+          <div className="grow pt-0.5">
+            <h3 className="flex gap-x-1.5 font-semibold text-gray-800">
+              Release v5.2.0 quick bug fix
+            </h3>
+          </div>
+        </div>
+        <div className="flex gap-x-3" onClick={() => handleStepClick(1)}>
+          <div className="relative z-10 w-7 h-7 flex justify-center items-center">
+            <div
+              className={`w-2 h-2 rounded-full bg-white border-2  group-hover:border-gray-600 ${
+                Number(scrollProgress) >= 1
+                  ? "border-blue-700"
+                  : "border-gray-300"
+              }`}
+            ></div>
+          </div>
+
+          <div className="grow pt-0.5">
+            <h3 className="flex gap-x-1.5 font-semibold text-gray-800">
+              Release v5.2.0 quick bug fix
+            </h3>
+          </div>
+        </div>
       </div>
     </main>
   );
